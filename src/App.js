ef82f2c4858e0c4d32873181c1f8b41dd281a50e
harpreet9891 <hs370378@gmail.com>
2023-05-18 13:44:37 +0530

    Added mockdata as well as App css js files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { mockData } from './mockData';
 
 function App() {
+  const[tab, setTab] = useState(1);
+  function activeTab(tabNumber){
+    setTab(tabNumber);
+  }
+  const ipl_teams = mockData.filter((team)=> team.item === "IPL Team");
+  const cricket_bats = mockData.filter((team)=> team.item === "Cricket Bats");
+  const cricket_balls = mockData.filter((team)=> team.item === "Cricket Balls");
+
+  const data = tab === 1 ? ipl_teams : tab === 2 ? cricket_bats : tab === 3 ? cricket_balls : null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='category'>
+          <div className='cricket_info'>
+            <div class="cricket-item" onClick={()=> activeTab(1)} style={{borderBottom: tab === 1 ? "5px solid yellow": ""}}>IPL Team</div>
+            <div class="cricket-item" onClick={()=> activeTab(2)} style={{borderBottom: tab === 2 ? "5px solid yellow": ""}}>Cricket Bats</div>
+            <div class="cricket-item" onClick={()=> activeTab(3)} style={{borderBottom: tab === 3 ? "5px solid yellow": ""}}>Cricket Balls</div>
+          </div>
+        </div>
+        <div className='item_div'>
+            {data && data.map((value)=>{
+              return(
+                <div><span>{value.description}</span></div>
+              )
+            })}
+        </div>
     </div>
   );
 }
---
+++ src/mockData.js
@@ -0,0 +1,32 @@
+export const mockData = [
+    {
+        id: 1,
+        item: "IPL Team",
+        description: "PBKS"
+    },
+    {
+        id: 2,
+        item: "IPL Team",
+        description: "CSK"
+    },
+    {
+        id: 3,
+        item: "Cricket Bats",
+        description: "MRF"
+    },
+    {
+        id: 4,
+        item: "Cricket Bats",
+        description: "Spartan"
+    },
+    {
+        id: 5,
+        item: "Cricket Balls",
+        description: "Kookaburra"
+    },
+    {
+        id: 6,
+        item: "Cricket Balls",
+        description: "COSCO"
+    }
+];
